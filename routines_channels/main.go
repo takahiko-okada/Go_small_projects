75e0283f860ad7f3b057b70a8ca6fc1854e5eed2
Takahiko Okada <takahikookada@gmail.com>
2018-04-20 16:15:16 +0200

    channel function

--- routines_channels/main.go
+++ routines_channels/main.go
@@ -15,20 +15,28 @@ func main() {
     "http://amazon.com",
   }
 
+  c := make(chan string)
+
   for _, link := range links {
-    go checkLink(link)
+    go checkLink(link, c)
     // go spawns new instance of the function in parallel
     // by default one instance, need to change the setting
     // if you want to use multiple thread.
   }
+
+  fmt.Println(<- c)
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
   _, err := http.Get(link)
   if err != nil {
     fmt.Println(link + "might be down.")
+    c <- "Might be down."
     return
   }
 
   fmt.Println(link, "is up.")
+  c <- "is up."
+
 }
