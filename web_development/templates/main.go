d061f7eb81514f7ad937e52bb606cf0d8a583aef
Takahiko Okada <takahikookada@gmail.com>
2018-05-27 23:11:28 +0200

    passing data using variable

--- web_development/templates/main.go
+++ web_development/templates/main.go
@@ -11,23 +11,28 @@ import (
 var tpl *template.Template
 
 func init() {
-  tpl = template.Must(template.ParseGlob("tpl/*"))
+  tpl = template.Must(template.ParseGlob("./web_development/templates/tpl/*"))
 }
 
 func main() {
   // Execute template, first one?
-  err := tpl.Execute(os.Stdout, nil)
+  err := tpl.Execute(os.Stdout, "is to find it.")
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  err = tpl.ExecuteTemplate(os.Stdout, "test0.gohtml", "is to find it.")
   if err != nil {
     log.Fatalln(err)
   }
 
   // Execute specific template with ExecuteTemplate func
-  err = tpl.ExecuteTemplate(os.Stdout, "Test1.gohtml", nil)
+  err = tpl.ExecuteTemplate(os.Stdout, "test1.gohtml", nil)
   if err != nil {
     log.Fatalln(err)
   }
   // Same as above
-  err = tpl.ExecuteTemplate(os.Stdout, "Test2.gohtml", nil)
+  err = tpl.ExecuteTemplate(os.Stdout, "test2.gohtml", nil)
   if err != nil {
     log.Fatalln(err)
   }
