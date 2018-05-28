b07d5c57f1c60cec7343fc77ef32f39cb8743c30
Takahiko Okada <takahikookada@gmail.com>
2018-05-28 15:51:34 +0200

    passing data into templates advanced

--- web_development/templates/main.go
+++ web_development/templates/main.go
@@ -10,6 +10,11 @@ import (
 // of *template(s) which are the memory addresses of the parsed template
 var tpl *template.Template
 
+type property struct {
+  Capital string
+  Weather string
+}
+
 func init() {
   tpl = template.Must(template.ParseGlob("./web_development/templates/tpl/*"))
 }
@@ -35,6 +40,57 @@ func main() {
   err = tpl.ExecuteTemplate(os.Stdout, "test2.gohtml", nil)
   if err != nil {
     log.Fatalln(err)
+  }
+
+
+  // passing composite literal
+    // {{range .}}
+      // <li>{{.}}</li>
+    // {{end}}
+  // range and end to display the data passed
+  sages := []string {"Ghandi", "Buddha", "Jesus"}
+  err = tpl.ExecuteTemplate(os.Stdout, "test3.gohtml", sages)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  example1 := map[string]string{
+    "Japan": "Sushi",
+    "China": "Noodle",
+    "Germany": "Wurst",
+    "France": "Pasta",
+  }
+
+  err1 := tpl.ExecuteTemplate(os.Stdout, "test3.gohtml", example1)
+  if err != nil {
+    log.Fatalln(err1)
+  }
+
+  germany := property{
+    Capital: "Berlin",
+    Weather: "Sunny",
+  }
+
+  japan := property{
+    Capital: "Tokyo",
+    Weather: "Rainy",
+  }
+
+  france := property{
+    Capital: "Paris",
+    Weather: "Cloudy",
+  }
+
+  country := tpl.ExecuteTemplate(os.Stdout, "test4.gohtml", germany)
+  if err != nil {
+    log.Fatalln(country)
+  }
+
+  countries :=[]property{germany, japan, france}
+
+  example2 := tpl.ExecuteTemplate(os.Stdout, "test5.gohtml", countries)
+  if err != nil {
+    log.Fatalln(example2)
   }
 }
 
